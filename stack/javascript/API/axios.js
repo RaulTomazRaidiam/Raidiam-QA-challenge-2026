fde0b98a494adc4d8d85fa6daaa0142d172a8620
RaulTomazRaidiam <raul.tomaz@raidiam.com>
2026-02-03 17:10:18 -0300

    removal of const

--- stack/javascript/API/axios.js
+++ stack/javascript/API/axios.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const { v4: uuidv4 } = require('uuid');
 const { expect } = require('chai');
-const { describe, it } = require('mocha');
 
 const BASE_URL = 'http://locahost:8080';
 const ACCOUNTS_ENDPOINT = '/account';
